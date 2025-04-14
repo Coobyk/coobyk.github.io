450613f2010a61f8dcacf6d7bd791665db57d497
Coobyk <110892785+Coobyk@users.noreply.github.com>
2025-04-14 13:54:22 +0200

    Revert "Try updating to next.js"
    This reverts commit 26108a51fbc57d7ae490026db45e4bb56d704268.

--- next-app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/coobyk.github.io',
-  assetPrefix: '/coobyk.github.io/',
-  trailingSlash: true,
-}
-
-module.exports = nextConfig
